72a98f436df8e45bb1f27deb5b0470cee20bd2e7
tulsyanshubham <141614975+tulsyanshubham@users.noreply.github.com>
2024-11-05 15:18:07 +0530

    upload

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div>
+      <div className="header py-3 flex flex-wrap justify-between items-center px-4 md:px-10 bg-white">
+        <div id="logo" className="font-bold text-base md:text-lg">
+          Khurak
+        </div>
+        <div className="">
+          <button className="bg-white text-green-500 font-bold text-sm md:text-lg py-2 px-6 md:px-12 rounded-lg border-2 border-green-500">
+            Join Us
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
