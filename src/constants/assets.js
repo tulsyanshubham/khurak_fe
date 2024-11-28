bc4f41c6641ca9575c339cf7d37df8619322b6a4
tulsyanshubham <141614975+tulsyanshubham@users.noreply.github.com>
2024-11-28 12:57:53 +0530

    old asset

--- src/constants/assets.js
+++ src/constants/assets.js
@@ -1,8 +1,8 @@
 export const assets = {
     logo: "/images/logo/logo.webp",
     logo_dark: "/images/logo/logo_dark.webp",
-    right_to_welcome: "/images/home/order.gif",
-    right_to_welcome_dark: "/images/home/order.gif",
+    right_to_welcome: "/images/home/order.svg",
+    right_to_welcome_dark: "/images/home/order_dark.svg",
     bmi: "/images/home/bmi.svg",
     icons: {
         call: "/images/icons/call.png",
