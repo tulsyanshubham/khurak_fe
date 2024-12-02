933b758265ef37ba8fb1fdc4c4498e91eeec21e7
tulsyanshubham <141614975+tulsyanshubham@users.noreply.github.com>
2024-12-02 20:09:28 +0530

    border adjusted

--- src/components/FloatingVideo.jsx
+++ src/components/FloatingVideo.jsx
@@ -13,7 +13,7 @@ export function FloatigVideo() {
                         Word From Founder
                     </span>
                 </div>
-                <div className="relative max-w-5xl aspect-video mx-auto w-full border-4 border-[#6C6C6C] p-2 md:p-6 bg-[#222222] rounded-[30px] shadow-2xl">
+                <div className="relative max-w-5xl aspect-video mx-auto w-full border-2 md:border-4 border-[#6C6C6C] p-2 md:p-6 bg-[#222222] rounded-[20px] md:rounded-[30px] shadow-2xl">
                     <div className="absolute md:h-3 h-1 md:w-3 w-1 rounded-full border-[1px] md:border-[3px] border-yellow-500 bg-black left-[2px] md:left-[6px] top-1/2 flex items-center justify-center" />
                     <div className="relatve h-full w-full overflow-hidden rounded-2xl bg-gray-100 dark:bg-zinc-900 md:rounded-2xl md:p-4">
                         <iframe className="w-full aspect-video rounded-lg" src="https://www.youtube.com/embed/djv50lQtI2E?si=ABqD2EHMH8DHDzVQ" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen={true} ></iframe>
