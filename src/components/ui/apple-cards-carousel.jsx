544b3419e797772109213a6d5cea395800056797
adiityakhare <aditiyakhare@mymudra.com>
2024-12-05 10:55:47 +0530

    Handle z-index on Order's now Modal

--- src/components/ui/apple-cards-carousel.jsx
+++ src/components/ui/apple-cards-carousel.jsx
@@ -19,7 +19,7 @@ import { useOutsideClick } from "@/hooks/use-outside-click";
 import { DirectionAwareHover } from "./direction-aware-hover";
 
 const CarouselContext = createContext({
-  onCardClose: () => { },
+  onCardClose: () => {},
   currentIndex: 0,
 });
 
@@ -74,16 +74,18 @@ export const Carousel = ({ items, initialScroll = 0 }) => {
     return window && window.innerWidth < 768;
   };
   return (
-    <CarouselContext.Provider value={{ onCardClose: handleCardClose, currentIndex }}>
-      <div className="relative w-full">
+    <CarouselContext.Provider
+      value={{ onCardClose: handleCardClose, currentIndex }}
+    >
+      <div className='relative w-full z-[0]'>
         <div
-          className="flex w-full overflow-x-scroll overscroll-x-auto sm:pt-5 pt-5 pb-8 md:py-30 scroll-smooth [scrollbar-width:none]"
+          className='flex w-full overflow-x-scroll overscroll-x-auto sm:pt-5 pt-5 pb-8 md:py-30 scroll-smooth [scrollbar-width:none]'
           ref={carouselRef}
           onScroll={checkScrollability}
         >
           <div
             className={cn(
-              "absolute right-0  z-[500] h-auto w-[5%] overflow-hidden bg-gradient-to-l"
+              "absolute right-0  z-[40] h-auto w-[5%] overflow-hidden bg-gradient-to-l"
             )}
           ></div>
 
@@ -110,27 +112,27 @@ export const Carousel = ({ items, initialScroll = 0 }) => {
                   },
                 }}
                 key={"card" + index}
-                className="last:pr-[1%] md:last:pr-[1.5%] rounded-3xl"
+                className='last:pr-[1%] md:last:pr-[1.5%] rounded-3xl'
               >
                 {item}
               </motion.div>
             ))}
           </div>
         </div>
-        <div className="hidden lg:flex justify-end gap-2 mr-10">
+        <div className='hidden lg:flex justify-end gap-2 mr-10'>
           <button
-            className="relative z-40 h-10 w-10 rounded-full bg-gray-100 flex items-center justify-center disabled:opacity-50"
+            className='relative z-40 h-10 w-10 rounded-full bg-gray-100 flex items-center justify-center disabled:opacity-50'
             onClick={scrollLeft}
             disabled={!canScrollLeft}
           >
-            <IconArrowNarrowLeft className="h-6 w-6 text-gray-500" />
+            <IconArrowNarrowLeft className='h-6 w-6 text-gray-500' />
           </button>
           <button
-            className="relative z-40 h-10 w-10 rounded-full bg-gray-100 flex items-center justify-center disabled:opacity-50"
+            className='relative z-40 h-10 w-10 rounded-full bg-gray-100 flex items-center justify-center disabled:opacity-50'
             onClick={scrollRight}
             disabled={!canScrollRight}
           >
-            <IconArrowNarrowRight className="h-6 w-6 text-gray-500" />
+            <IconArrowNarrowRight className='h-6 w-6 text-gray-500' />
           </button>
         </div>
       </div>
@@ -175,12 +177,12 @@ export const Card = ({ card, index, layout = false }) => {
     <>
       <AnimatePresence>
         {open && (
-          <div className="fixed inset-0 h-screen z-50 overflow-auto">
+          <div className='fixed inset-0 h-screen z-50 overflow-auto'>
             <motion.div
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
-              className="bg-black/80 backdrop-blur-lg h-full w-full fixed inset-0"
+              className='bg-black/80 backdrop-blur-lg h-full w-full fixed inset-0'
             />
             <motion.div
               initial={{ opacity: 0 }}
@@ -188,30 +190,32 @@ export const Card = ({ card, index, layout = false }) => {
               exit={{ opacity: 0 }}
               ref={containerRef}
               layoutId={layout ? `card-${card.title}` : undefined}
-              className="max-w-5xl mx-auto bg-white dark:bg-neutral-900 h-fit z-[60] my-10 p-4 md:p-10 rounded-3xl font-sans relative"
+              className='max-w-5xl mx-auto bg-white dark:bg-neutral-900 h-fit z-[60] my-10 p-4 md:p-10 rounded-3xl font-sans relative'
             >
               <button
-                className="sticky top-4 h-8 w-8 right-0 ml-auto bg-black dark:bg-white rounded-full flex items-center justify-center"
+                className='sticky top-4 h-8 w-8 right-0 ml-auto bg-black dark:bg-white rounded-full flex items-center justify-center'
                 onClick={handleClose}
               >
-                <IconX className="h-6 w-6 text-neutral-100 dark:text-neutral-900" />
+                <IconX className='h-6 w-6 text-neutral-100 dark:text-neutral-900' />
               </button>
               <motion.p
                 layoutId={layout ? `category-${card.title}` : undefined}
-                className="text-base font-medium text-black dark:text-white"
+                className='text-base font-medium text-black dark:text-white'
               >
                 {card.category}
               </motion.p>
               <motion.p
                 layoutId={layout ? `title-${card.title}` : undefined}
-                className="text-2xl md:text-5xl font-semibold text-neutral-700 mt-4 dark:text-white"
+                className='text-2xl md:text-5xl font-semibold text-neutral-700 mt-4 dark:text-white'
               >
-                <span dangerouslySetInnerHTML={{ __html: card.content.title }} />
+                <span
+                  dangerouslySetInnerHTML={{ __html: card.content.title }}
+                />
               </motion.p>
-              <div className="py-10">
-                <div className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4">
-                  <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
-                    <span className="font-bold text-neutral-700 dark:text-neutral-200">
+              <div className='py-10'>
+                <div className='bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4'>
+                  <p className='text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto'>
+                    <span className='font-bold text-neutral-700 dark:text-neutral-200'>
                       {card.content.head}
                     </span>{" "}
                     {card.content.desc}
@@ -219,10 +223,10 @@ export const Card = ({ card, index, layout = false }) => {
 
                   <Image
                     src={card.content.img_src}
-                    alt="Macbook mockup from Aceternity UI"
-                    height="500"
-                    width="500"
-                    className="md:w-1/2 w-full mx-auto aspect-video object-cover mt-5 rounded-lg"
+                    alt='Macbook mockup from Aceternity UI'
+                    height='500'
+                    width='500'
+                    className='md:w-1/2 w-full mx-auto aspect-video object-cover mt-5 rounded-lg'
                   />
                 </div>
               </div>
@@ -233,20 +237,20 @@ export const Card = ({ card, index, layout = false }) => {
       <motion.button
         layoutId={layout ? `card-${card.title}` : undefined}
         onClick={handleOpen}
-        className="rounded-3xl bg-gray-100 dark:bg-neutral-900 h-52 w-32 md:h-[18rem] md:w-52 overflow-hidden flex flex-col items-start justify-start relative"
+        className='rounded-3xl bg-gray-100 dark:bg-neutral-900 h-52 w-32 md:h-[18rem] md:w-52 overflow-hidden flex flex-col items-start justify-start relative'
       >
-        <div className="block lg:hidden">
-          <div className="absolute h-full top-0 inset-x-0 bg-gradient-to-b from-black/50 via-black/50 to-transparent z-30 pointer-events-none" />
-          <div className="relative z-40 p-4">
+        <div className='block lg:hidden'>
+          <div className='absolute h-full top-0 inset-x-0 bg-gradient-to-b from-black/50 via-black/50 to-transparent z-30 pointer-events-none' />
+          <div className='relative z-40 p-4'>
             <motion.p
               layoutId={layout ? `category-${card.category}` : undefined}
-              className="text-white text-xs md:text-sm font-medium font-sans text-left"
+              className='text-white text-xs md:text-sm font-medium font-sans text-left'
             >
               {card.category}
             </motion.p>
             <motion.p
               layoutId={layout ? `title-${card.title}` : undefined}
-              className="text-white text-sm md:text-xl font-semibold max-w-xs text-left [text-wrap:balance] font-sans mt-2"
+              className='text-white text-sm md:text-xl font-semibold max-w-xs text-left [text-wrap:balance] font-sans mt-2'
             >
               {card.title}
             </motion.p>
@@ -255,21 +259,24 @@ export const Card = ({ card, index, layout = false }) => {
             src={card.src}
             alt={card.title}
             fill
-            className="object-cover absolute z-10 inset-0"
+            className='object-cover absolute z-10 inset-0'
           />
         </div>
         {/* <div className="hidden md:block absolute h-full top-0 inset-x-0"> */}
-        <DirectionAwareHover imageUrl={card.src} className="h-full inset-x-0 hidden lg:block absolute top-0" >
-          <div className="relative px-4 py-10">
+        <DirectionAwareHover
+          imageUrl={card.src}
+          className='h-full inset-x-0 hidden lg:block absolute top-0'
+        >
+          <div className='relative px-4 py-10'>
             <motion.p
               layoutId={layout ? `category-${card.category}` : undefined}
-              className="text-white text-xs md:text-sm font-medium font-sans text-left"
+              className='text-white text-xs md:text-sm font-medium font-sans text-left'
             >
               {card.category}
             </motion.p>
             <motion.p
               layoutId={layout ? `title-${card.title}` : undefined}
-              className="text-white text-lg md:text-xl font-semibold max-w-xs text-left [text-wrap:balance] font-sans mt-2"
+              className='text-white text-lg md:text-xl font-semibold max-w-xs text-left [text-wrap:balance] font-sans mt-2'
             >
               {card.title}
             </motion.p>
@@ -295,8 +302,8 @@ const BlurImage = ({ height, width, src, className, alt, ...rest }) => {
       src={src}
       width={width}
       height={height}
-      loading="lazy"
-      decoding="async"
+      loading='lazy'
+      decoding='async'
       blurDataURL={typeof src === "string" ? src : undefined}
       alt={alt ? alt : "Background of a beautiful view"}
       {...rest}
